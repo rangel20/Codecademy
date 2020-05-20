61216cf67782c0ab412fb8f8a33604e2bf9a478f
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-20 03:24:29 -0300

    Declared variables to do the convertion calculation of temperatura.

---
+++ JS-Projects/Kelvin-Weather/toFahrenheit.js
@@ -0,0 +1,6 @@
+// This variable shows de default Kelvin temperature.
+const kelvin = 293;
+// The celcius variable receives the value of Kelvin - 273.
+const celcius = kelvin - 273;
+// This variable receives the fahrenheit equation result.
+let fahrenheit = celcius * (9/5) + 32;
