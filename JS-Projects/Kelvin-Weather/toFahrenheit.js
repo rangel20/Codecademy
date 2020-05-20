254ff298c0327198c226340ace9e5030e5c2164a
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-20 03:31:53 -0300

    Added equatiosn to calculate in Newton and Fahrenheit. The project is finished.

--- JS-Projects/Kelvin-Weather/toFahrenheit.js
+++ JS-Projects/Kelvin-Weather/toFahrenheit.js
@@ -3,4 +3,7 @@ const kelvin = 293;
 // The celcius variable receives the value of Kelvin - 273.
 const celcius = kelvin - 273;
 // This variable receives the fahrenheit equation result.
-let fahrenheit = celcius * (9/5) + 32;
+const fahrenheit = Math.floor(celcius * (9 / 5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+const newton = Math.floor(celcius * (33 / 100));
+console.log(`The temperature is ${newton} degrees Newton.`);
