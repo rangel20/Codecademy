5c152a5ea56ca6e679e3000bd5820c9c104fd124
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-24 01:54:47 -0300

    Calculating my age in dog years.

---
+++ JS-Projects/Dog-Years/dog-Years.js
@@ -0,0 +1,5 @@
+const myAge = 23; // This is my age as an integer number.
+let earlyYears = 2; // This variable will have it's value changed.
+earlyYears *= 10.5;
+let laterYears = myAge - 2; // Subtrated 2 from my age, to calculate a dig's age.
+laterYears *= 4; // Calculating the rest of the dog's age.
