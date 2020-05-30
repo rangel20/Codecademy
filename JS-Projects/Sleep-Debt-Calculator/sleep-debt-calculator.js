4fe04b9d3c734894e8a0e4be85505a478c1d3a2c
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-30 01:48:04 -0300

    Project finished..

--- JS-Projects/Sleep-Debt-Calculator/sleep-debt-calculator.js
+++ JS-Projects/Sleep-Debt-Calculator/sleep-debt-calculator.js
@@ -1,31 +1,26 @@
-const hours = () => Math.floor(Math.random() * 11);
-
-const getSleepHours = (day) => {
-  switch (day) {
-    case 'monday':
-      return hours();
-    case 'tuesday':
-      return hours();
-    case 'wednesday':
-      return hours();
-    case 'thursday':
-      return hours();
-    case 'friday':
-      return hours();
-    case 'saturday':
-      return hours();
-    case 'sunday':
-      return hours();
-  }
-};
+const hours = () => 11 - Math.floor(Math.random() * 8);
 
 const getActualSleepHours = () => {
   const weekDays = ['sunday', 'monday', 'tuesday',
     'wednesday', 'thursday', 'friday', 'saturday'];
-  const sleepHours = weekDays.reduce((r, d) => r + getSleepHours(d), 0);
+  const sleepHours = weekDays.reduce(r => r + hours(), 0);
+  return sleepHours;
 };
 
-const getIdealSleepHours = () => {
-  const idealHours = 10;
-  return idealHours * 7;
+const getIdealSleepHours = ideal => ideal * 7;
+
+const calculateSleepDebt = ideal => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours(ideal);
+  if (actualSleepHours === idealSleepHours) {
+    console.log('You got the perfect amount of sleep');
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log('You got more sleep than needed');
+  } else {
+    console.log(`You should get some rest.
+    You need to sleep more ${idealSleepHours - actualSleepHours}`);
+  }
+  // console.log(actualSleepHours);
 };
+
+calculateSleepDebt(8);
