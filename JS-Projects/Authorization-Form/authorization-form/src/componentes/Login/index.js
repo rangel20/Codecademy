a61d61cfbb3eaad1338ce4ed1ba77f12d79a8cfc
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-15 00:32:50 -0300

    Styling components.

--- JS-Projects/Authorization-Form/authorization-form/src/componentes/Login/index.js
+++ JS-Projects/Authorization-Form/authorization-form/src/componentes/Login/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Login extends Component {
   render() {
     const { authorize } = this.props;
     return (
-      <div>
+      <div className="login-form">
         <h1>Enter the Password</h1>
         <form action="#" onSubmit={authorize}>
           <input type="password" placeholder="Password" />
